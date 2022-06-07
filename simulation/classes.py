57541bdbe06e9fe8f460ee3fd4e033e0102e3292
FStiffler <fstiffler@gmx.ch>
2022-06-07 23:58:37 +0200

    Addmethod to PlayerPool class to allow the extraction of player data (id, skill and salary) as dataframe

--- simulation/classes.py
+++ simulation/classes.py
@@ -1,18 +1,34 @@
 from parameters import *
 import numpy as np
+import pandas as pd
 
 
 # define player pool as class
 class PlayerPool(object):
-    def __init__(self, size=k_new):
+    def __init__(self):
         '''
         Initializes the player pool object
-        size (int): Integer defining the number of players in the pool, default = k_new
+        The object is fully initialised based on parameters
         A player pool object has the following attributes:
-            self.size (int): determined by size
+            self.size (int): determines pool size (number of available players)
             self.playerID (numpy array): determines the ID's of players
             self.playerSkill (numpy array): determines the skill level of players
+            self.playerSalary (numpy array): determines the salary of players
         '''
-        self.size = size
-        self.playerID = np.arange(1, k_new+1)
-        self.playerSkill = np.random.beta(alpha, beta, self.size)
+        self.size = k_new
+        self.playerID = np.arange(start=1, stop=k_new + 1)  # create id's from 1 to k_new
+        self.playerSkill = np.round(np.random.beta(a=alpha, b=beta, size=k_new), 2)  # draw skill from beta distribution
+        self.playerSalary = np.round(w_max * self.playerSkill * (1 - ((k_new - k_old) / k_old)))  # calculate salary
+
+    def get_data(self):
+        '''
+        Get player pool as data set
+        Returns: player_data (pandas dataframe)
+        '''
+        player_data = pd.DataFrame(
+            data=np.column_stack((self.playerID, self.playerSkill, self.playerSalary)),  # arrays as columns
+            columns=["ID", "Skill", "Salary"]
+        )
+        player_data = player_data.astype({"ID": int})  # change ID to integer
+
+        return player_data
--- simulation/parameters.py
+++ simulation/parameters.py
@@ -4,4 +4,5 @@ k_old = 308  # player pool size before measures
 k_new = 308  # player pool size after measures
 alpha = 5  # parameter alpha of beta distribution
 beta = 5  # parameter beta of beta distribution
+w_max = 100000  # maximal salary for best available player
 h = 22  # team size
