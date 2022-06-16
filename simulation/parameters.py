447f2cebaa8fbe5cb129d2da2e1f2f2433555a64
FStiffler <fstiffler@gmx.ch>
2022-06-16 03:07:46 +0200

    Add initialisation of teams

--- simulation/parameters.py
+++ simulation/parameters.py
@@ -8,3 +8,9 @@ k_new = n*h  # player pool size after measures
 alpha = 5  # parameter alpha of beta distribution
 beta = 5  # parameter beta of beta distribution
 
+# list of teams
+teams = ["team_"+str(i+1) for i in range(n)]
+
+# list of revenues
+revenues = [1400000, 1300000, 1200000, 1100000, 1000000, 900000, 800000,
+            700000, 600000, 500000, 400000, 300000, 200000, 100000]
--- simulation/simulation.py
+++ simulation/simulation.py
@@ -1,10 +1,12 @@
+from functions import select_players
 from classes import *
-from functions import *
-
 
 # initialise the player pool
 playerPool = PlayerPool()
 
+# initialise teams
+for i in range(len(teams)):
+    globals()[teams[i]] = Team(teams[i], revenues[i])
+
 # select players
 selectedPlayers = select_players(playerPool)
-
